961b373b01bdf4c46610b0be0bd5e361af9c6e78
Javierimperial <javiersanchezugalde@gmail.com>
2019-04-24 08:07:52 -0600

    added fire base
    added fire base

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+import logo from './logo.svg';
 import './App.css';
-import firebase from './firebase';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.ref = firebase.firestore().collection('boards');
-    this.state = {};
-  }
-
   render() {
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
+
     return (
-      <div className="container"></div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {
+            user
+              ? <p>Hello, {user.displayName}</p>
+              : <p>Please sign in.</p>
+          }
+
+          {
+            user
+              ? <button onClick={signOut}>Sign out</button>
+              : <button onClick={signInWithGoogle}>Sign in with Google</button>
+          }
+        </header>
+      </div>
     );
   }
 }
 
-export default App;
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDM61HS0dEVHTnUF6GLteiPk_DRLXr8Ejo",
+    authDomain: "whattocook-a7b73.firebaseapp.com",
+    databaseURL: "https://whattocook-a7b73.firebaseio.com",
+    projectId: "whattocook-a7b73",
+    storageBucket: "whattocook-a7b73.appspot.com",
+    messagingSenderId: "514570919508"
+};
