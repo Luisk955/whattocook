1c12584741a4de78c131c37e1086daabc541501b
Luisk <ldelvalleu@ucenfotec.ac.cr>
2019-04-24 21:35:56 -0600

    add bs
    add bs
    change styles

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import withFirebaseAuth from 'react-with-firebase-auth'
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import firebaseConfig from './firebaseConfig';
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
@@ -15,25 +15,53 @@ class App extends Component {
       signOut,
       signInWithGoogle,
     } = this.props;
+    this.state = {
+      newAccName: '',
+      newAccEmail: '',
+      newAccPass: ''
+    };
+
+
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="text-center mt-5">
+        <header id="headerContainer">
           {
-            user
-              ? <p>Hello, {user.displayName}</p>
-              : <p>Please sign in.</p>
+            user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>
           }
 
           {
-            user
-              ? <button onClick={signOut}>Sign out</button>
-              : <button onClick={signInWithGoogle}>Sign in with Google</button>
+            user ? <button onClick={signOut} className="btn btn-primary">Sign out</button> : <button onClick={signInWithGoogle} className="btn btn-primary">Sign in with Google</button>
           }
+          <br/>
+          <button className="btn btn-primary mt-2" onClick={openForm}>Create account</button>
         </header>
-      </div>
+          <form className="mt-5 container d-none" id="formContainer">
+            <div className="form-group col-3 mx-auto">
+              <label htmlFor="newAccName">Name</label>
+              <input type="text" className="form-control" id={this.state.newAccName} placeholder="Name"/>
+            </div>
+            <div className="form-group col-3 mx-auto">
+              <label htmlFor="newAccEmail">Email</label>
+              <input type="email" className="form-control" id={this.state.newAccEmail} placeholder="Email"/>
+            </div>
+            <div className="form-group col-3 mx-auto">
+              <label htmlFor="newAccPass">Password</label>
+              <input type="password" className="form-control" id={this.state.newAccPass} placeholder="Password"/>
+            </div>
+            <button type="submit" className="btn btn-primary" onClick={this.createAccount}>Create account</button>
+            <button type="submit" className="ml-2 btn btn-danger" onClick={cancel}>Cancel</button>
+          </form>
+        </div>
     );
+
+    this.createAccount= (e) =>{
+      e.preventDefault();
+      alert(document.getElementById('newAccName').vaulue);
+      alert(document.getElementById('newAccEmail').vaulue);
+      alert(document.getElementById('newAccPass').vaulue);
+    
+    }
   }
 }
 
@@ -47,3 +75,14 @@ export default withFirebaseAuth({
   providers,
   firebaseAppAuth,
 })(App);
+
+
+function openForm(){
+  document.getElementById(`headerContainer`).classList.add(`d-none`);
+  document.getElementById(`formContainer`).classList.remove(`d-none`);
+}
+
+function cancel(){
+  document.getElementById(`formContainer`).classList.add(`d-none`);
+  document.getElementById(`headerContainer`).classList.remove(`d-none`);
+}
