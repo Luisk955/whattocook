8d88d0e61c12c40fb425954c79753b8b0f929746
Luisk <ldelvalleu@ucenfotec.ac.cr>
2019-04-25 18:19:35 -0600

    nav fix

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -34,14 +34,17 @@ const NavigationAuth = () => (
 );
 
 const NavigationNonAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-    </li>
-  </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav">
+        <Link className="nav-item nav-link" to={ROUTES.LANDING}>Landing</Link>
+        <Link className="nav-item nav-link" to={ROUTES.SIGN_IN}>Sign In</Link>
+      </div>
+    </div>
+    </nav>
 );
 
 export default Navigation;
